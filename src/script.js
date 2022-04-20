081fc981bae09f93ce30e78a8f352230b3ff45cf
jonatas-melo-silva <jonatas891024@gmail.com>
2022-04-20 01:00:16 -0300

    :sparkles: Adiciona movimento para a cobrinha
    Define lógica que faz a cobrinha se movimentar em um determinado tempo

--- src/script.js
+++ src/script.js
@@ -1,23 +1,57 @@
-let canvas = document.getElementById("canvas");
-let context = canvas.getContext("2d");
-let box = 32;
-let snake = [];
+let canvas = document.getElementById('canvas')
+let context = canvas.getContext('2d')
+let box = 32
+let snake = []
 snake[0] = {
-    x: 8 * box,
-    y: 8 * box
+  x: 8 * box,
+  y: 8 * box
 }
 
+let direction = 'right'
+
 function createBackground() {
-    context.fillStyle = "#000";
-    context.fillRect(0, 0, box * 16, box * 16);
+  // create the background
+  context.fillStyle = '#000'
+  context.fillRect(0, 0, box * 16, box * 16)
 }
 
 function createSnake() {
-    for (const element of snake) {
-        context.fillStyle = "#0f0";
-        context.fillRect(element.x, element.y, box, box);
-    }
+  // create the snake
+  for (const element of snake) {
+    context.fillStyle = '#0f0'
+    context.fillRect(element.x, element.y, box, box)
+  }
 }
 
-createBackground();
-createSnake();
+function startGame() {
+  // start game by drawing the snake and the background
+  createBackground()
+  createSnake()
+
+  let snakePositionX = snake[0].x
+  let snakePositionY = snake[0].y
+
+  if (direction === 'right') {
+    snakePositionX += box
+  }
+  if (direction === 'left') {
+    snakePositionX -= box
+  }
+  if (direction === 'up') {
+    snakePositionY -= box
+  }
+  if (direction === 'down') {
+    snakePositionY += box
+  }
+
+  snake.pop()
+
+  let newHead = {
+    x: snakePositionX,
+    y: snakePositionY
+  }
+
+  snake.unshift(newHead)
+}
+
+let game = setInterval(startGame, 100) // 100ms = 10fps
