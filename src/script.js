6f45a1a2619d226661b5598fa758534117b0a86e
jonatas-melo-silva <jonatas891024@gmail.com>
2022-04-20 00:35:48 -0300

    :sparkles: Adiciona função que cria a cobrinha
    Desenha a cobrinha no centro da tela

--- src/script.js
+++ src/script.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function createBackground() {
     context.fillStyle = "#000";
     context.fillRect(0, 0, box * 16, box * 16);
 }
 
+function createSnake() {
+    for (const element of snake) {
+        context.fillStyle = "#0f0";
+        context.fillRect(element.x, element.y, box, box);
+    }
+}
+
 createBackground();
+createSnake();
