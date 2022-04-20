4b1b8ed5962a595419920eb641e6cf4713d80637
jonatas-melo-silva <jonatas891024@gmail.com>
2022-04-20 01:30:41 -0300

    :sparkles: Adiciona controles e impede que a cobra suma
    Deifine função para controlar a direção da cobrinha e faz com que a cobra aparece do outro lado da tela

--- src/script.js
+++ src/script.js
@@ -28,9 +28,22 @@ function startGame() {
   createBackground()
   createSnake()
 
+  // make the snake appear on the other side of the screen
+  if (snake[0].x > 15 * box && direction === 'right') {
+    snake[0].x = 0
+  } else if (snake[0].x < 0 && direction === 'left') {
+    snake[0].x = 16 * box
+  } else if (snake[0].y > 15 * box && direction === 'down') {
+    snake[0].y = 0
+  } else if (snake[0].y < 0 && direction === 'up') {
+    snake[0].y = 16 * box
+  }
+
+
   let snakePositionX = snake[0].x
   let snakePositionY = snake[0].y
 
+  // move the snake
   if (direction === 'right') {
     snakePositionX += box
   }
@@ -55,3 +68,35 @@ function startGame() {
 }
 
 let game = setInterval(startGame, 100) // 100ms = 10fps
+
+function handleKeyboard(event) {
+  console.log(event.keyCode)
+  // handle keyboard input
+  if (event.keyCode === 37 && direction !== 'right') {
+    direction = 'left'
+  } else if (event.keyCode === 38 && direction !== 'down') {
+    direction = 'up'
+  } else if (event.keyCode === 39 && direction !== 'left') {
+    direction = 'right'
+  } else if (event.keyCode === 40 && direction !== 'up') {
+    direction = 'down'
+  } else if (event.keyCode === 80) {
+    handlePause( )
+  } else if (event.keyCode === 82) {
+    handleResume( )
+  }
+}
+
+function handlePause() {
+  // pause the game
+  clearInterval(game)
+}
+
+function handleResume() {
+  // resume the game
+  game = setInterval(startGame, 100)
+}
+
+document.addEventListener('keydown', handleKeyboard)
+document.getElementById('pause').addEventListener('click', handlePause)
+document.getElementById('resume').addEventListener('click', handleResume) 
